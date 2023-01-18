5b8b32fb02bb607c3356b06ecbbf607e12355156
fengweiping <fengweiping@jd.com>
2023-01-18 18:39:45 +0800

    feat:fetch

---
+++ src/17.Fetch-async-awaite/index.tsx
@@ -0,0 +1,47 @@
+import { nanoid } from "nanoid";
+import React, { useState, useEffect } from "react";
+
+type listType = {
+  name: string,
+  data: {
+    cbf: string,
+    id: string,
+    m: string;
+    op: string,
+    p: string,
+  }[]
+};
+type AppProps = {};
+
+export const App: React.FC<AppProps> = () => {
+  const [list, setlist] = useState<listType>();
+  const initial = () => {
+    fetch("/studio/mock/data.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setlist(json);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  useEffect(() => {
+    initial();
+  }, []);
+  return (
+    <div>
+      {!!list ? (
+        <div>
+          {list.data.map((item) => {
+            return <li key={item.id}>${item.op}</li>;
+          })}
+        </div>
+      ) : (
+        "没有数据"
+      )}
+    </div>
+  );
+};
+export default App;
