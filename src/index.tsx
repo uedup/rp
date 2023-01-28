2ae4f13b260d299c33c60fc113baff19b9869eb0
fengweiping <fengweiping@jd.com>
2023-01-28 20:38:59 +0800

    feat:fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Demo from './30.renderProps';
+import Demo from './31.Fragment';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
